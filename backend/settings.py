ddb17669bf865a29a5f333f265195186e12a2a00
paulr <paulriff.prb@gmail.com>
2025-10-15 20:57:46 -0400

    Configuracion del movil correctamente

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-k_hm7dl%s+fisxqs8xnp+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,10.0.2.2').split(',')
 
 # Application definition
 
