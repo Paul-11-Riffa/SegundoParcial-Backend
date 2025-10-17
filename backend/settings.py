a0667f81316d6689f6dc8410c3669e291230d331
paulr <paulriff.prb@gmail.com>
2025-10-17 19:10:35 -0400

    Filtros de venta implementados

--- backend/settings.py
+++ backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'django_filters',
     'api',
     'products',
     'sales',
@@ -144,6 +145,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'UNAUTHENTICATED_USER': None,
     'UNAUTHENTICATED_TOKEN': None,
     'DEFAULT_PARSER_CLASSES': [
