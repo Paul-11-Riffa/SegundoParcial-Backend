8e0f22a942ffe7229f699c35d4f4536b1d752f83
paulr <paulriff.prb@gmail.com>
2025-10-17 10:56:17 -0400

    Configutacion de pagos con stripe implementado correctamente

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+# Registra los modelos para que aparezcan en el panel de admin
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- sales/urls.py
+++ sales/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import CartView, CartItemView
+from .views import CartView, CartItemView, StripeCheckoutView, StripeWebhookView, ManualOrderCompletionView
 
 urlpatterns = [
     # URL para ver el carrito y añadir artículos
     path('cart/', CartView.as_view(), name='cart'),
     # URL para actualizar o eliminar un artículo específico por su ID
     path('cart/items/<int:item_id>/', CartItemView.as_view(), name='cart-item'),
+    path('checkout/', StripeCheckoutView.as_view(), name='checkout'),
+    path('stripe-webhook/', StripeWebhookView.as_view(), name='stripe-webhook'),
+    path('debug/complete-order/', ManualOrderCompletionView.as_view(), name='debug-complete-order'),
+
 ]
